eb70dcc5854ee58863ffac297e02fdf27add2a9b
carlos diniz <cca.diniz@gmail.com>
2013-08-06 16:20:07 +0000

    cenas

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var buffer = new Buffer(256);
 app.use(express.logger());
+
+fs.readFile('index.html', funciton (err, buffer); 
 
 app.get('/', function(request, response) {
   response.send('Hello World q		2iqctq!');
+  response.send(buffer.toString("utf-8");	
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+)}
