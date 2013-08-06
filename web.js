298fe0d8f646eae6417fc6f2d04a4f88361b23d4
DinizPT <cca.diniz@gmail.com>
2013-08-06 17:57:23 +0100

    Update web.js

--- web.js
+++ web.js
@@ -1,18 +1,18 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
-var buffer = new Buffer(256);
-app.use(express.logger());
+
+var app = express();
 
 var mybuf;
+app.use(express.logger());
 
-mybuf = fs.readFileSync('index.html');
+ mybuf=fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World q		2iqctq!<br>' mybuf.toString());
+  response.send(mybuf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-)};
+});
